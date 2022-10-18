c7216ee9d8ebbcc8c26281f5f811059a84627635
SGCMarkus <markusornik@gmail.com>
2022-10-18 20:46:55 +0200

    pal: create libpal_headers module
    sound effects depend on pal headers
    export them this way even if we dont build libar-pal on OSS builds

--- Android.mk
+++ Android.mk
@@ -5,6 +5,19 @@ ifeq ($(TARGET_USES_QCOM_AUDIO_AR),true)
 LOCAL_PATH := $(call my-dir)
 PAL_BASE_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE                := libpal_headers
+LOCAL_VENDOR_MODULE         := true
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+    $(LOCAL_PATH)/stream/inc \
+    $(LOCAL_PATH)/device/inc \
+    $(LOCAL_PATH)/session/inc \
+    $(LOCAL_PATH)/resource_manager/inc \
+    $(LOCAL_PATH)/context_manager/inc \
+    $(LOCAL_PATH)/utils/inc \
+    $(LOCAL_PATH)/plugins/codecs
+include $(BUILD_HEADER_LIBRARY)
+
 ifneq ($(QCPATH),)
 
 include $(CLEAR_VARS)
@@ -41,13 +54,6 @@ LOCAL_CFLAGS        += -DA2DP_SINK_SUPPORTED
 endif
 
 LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/stream/inc \
-    $(LOCAL_PATH)/device/inc \
-    $(LOCAL_PATH)/session/inc \
-    $(LOCAL_PATH)/resource_manager/inc \
-    $(LOCAL_PATH)/context_manager/inc \
-    $(LOCAL_PATH)/utils/inc \
-    $(LOCAL_PATH)/plugins/codecs \
     $(TOP)/system/media/audio_route/include \
     $(TOP)/system/media/audio/include
 
@@ -139,7 +145,8 @@ LOCAL_HEADER_LIBRARIES := \
     libagm_headers \
     libacdb_headers \
     libarosal_headers \
-    libvui_dmgr_headers
+    libvui_dmgr_headers \
+    libpal_headers
 
 LOCAL_SHARED_LIBRARIES := \
     libar-gsl\
